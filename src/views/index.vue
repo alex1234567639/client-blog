5ef05e90230205b24cda3153fdd4c44c9860d434
alex1234567639 <alex8603062000@gmail.com>
2021-08-28 17:39:50 +0800

    [feature]配置footer、navbar組件

---
+++ src/views/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Navbar />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar/index'
+import Footer from '@/components/Footer/index'
+
+export default {
+    components: {
+        Navbar,
+        Footer
+    }
+}
+</script>
