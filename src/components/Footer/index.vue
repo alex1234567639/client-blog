0df5f9b7edcae357c2fa3d6ec8fd746d53f31012
alex1234567639 <alex8603062000@gmail.com>
2021-08-29 17:13:45 +0800

    [feature]footer切版

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        Footer
+    <div class="footer-wrapper">
+        <div class="line"></div>
+        <div class="text">Yu-Gi-Oh Blog</div>
     </div>
 </template>
 
@@ -9,3 +10,30 @@ export default {
     
 }
 </script>
+
+<style lang="scss" scoped>
+.footer-wrapper {
+    position: relative;
+    width: 100%;
+    height: 10vw;
+    background-color: #000000;
+    color: #ffffff;
+    .line {
+        position: absolute;
+        width: 90%;
+        height: 1px;
+        background-color: lightgray;
+        top: 5vw;
+        left: 5%;
+    }
+    .text {
+        position: absolute;
+        top: 5vw;
+        left: 50%;
+        transform: translateY(-50%) translateX(-50%);
+        padding: 0 4vw;
+        text-align: center;
+        background-color: #000000;
+    }
+} 
+</style>
