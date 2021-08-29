f59e9bf7361b07c7817626e2f4a313db2bb35e8e
alex1234567639 <alex8603062000@gmail.com>
2021-08-29 22:38:05 +0800

    [modify]調整footer高

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="footer-wrapper">
         <div class="line"></div>
-        <div class="text">Yu-Gi-Oh Blog</div>
+        <div class="text">Yu-Gi-Oh 部落格</div>
     </div>
 </template>
 
@@ -15,7 +15,7 @@ export default {
 .footer-wrapper {
     position: relative;
     width: 100%;
-    height: 10vw;
+    height: 8vw;
     background-color: #000000;
     color: #ffffff;
     .line {
@@ -23,15 +23,15 @@ export default {
         width: 90%;
         height: 1px;
         background-color: lightgray;
-        top: 5vw;
+        top: 4vw;
         left: 5%;
     }
     .text {
         position: absolute;
-        top: 5vw;
+        top: 4vw;
         left: 50%;
         transform: translateY(-50%) translateX(-50%);
-        padding: 0 4vw;
+        padding: 0 3vw;
         text-align: center;
         background-color: #000000;
     }
